8f3898bfd3c3a094600766c5ab60e5a9c33e02cf
M Sadiq Warsi <157520386+msw2005@users.noreply.github.com>
2025-10-03 10:44:45 +0100

    Update CNN Model for Diagnosis.py

--- CNN Model for Diagnosis.py
+++ CNN Model for Diagnosis.py
@@ -14,7 +14,7 @@ train_generator = train_datagen.flow_from_directory(
     class_mode='binary'
 )
 
-# Option 2: Escaping the backslashes
+# Option 2:Escaping the backslashes
 val_generator = val_datagen.flow_from_directory(
     'C:\\Users\\Ali\\OneDrive - University of Greenwich\\Pictures\\archive (6)\\val',
     target_size=(64, 64),
